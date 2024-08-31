8c238bb6ead9a93ff2ffb4467201943d4d61e22f
oliver <oliver.joseph.johnson@gmail.com>
2024-08-31 09:38:29 -0400

    Code

---
+++ src/main/java/net/fabricmc/installer/client/ClientInstaller.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.installer.client;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.MessageFormat;
+
+import mjson.Json;
+
+import net.fabricmc.installer.LoaderVersion;
+import net.fabricmc.installer.util.FabricService;
+import net.fabricmc.installer.util.InstallerProgress;
+import net.fabricmc.installer.util.Library;
+import net.fabricmc.installer.util.Reference;
+import net.fabricmc.installer.util.Utils;
+
+public class ClientInstaller {
+	public static String install(Path mcDir, String gameVersion, LoaderVersion loaderVersion, InstallerProgress progress) throws IOException {
+		System.out.println("Installing " + gameVersion + " with fabric " + loaderVersion.name);
+
+		String profileName = String.format("%s-%s-%s", Reference.LOADER_NAME, loaderVersion.name, gameVersion);
+
+		Path versionsDir = mcDir.resolve("versions");
+		Path profileDir = versionsDir.resolve(profileName);
+		Path profileJson = profileDir.resolve(profileName + ".json");
+
+		if (!Files.exists(profileDir)) {
+			Files.createDirectories(profileDir);
+		}
+
+		Path dummyJar = profileDir.resolve(profileName + ".jar");
+		Files.deleteIfExists(dummyJar);
+		Files.createFile(dummyJar);
+
+		Json json = FabricService.queryMetaJson(String.format("v2/versions/loader/%s/%s/profile/json", gameVersion, loaderVersion.name));
+		Files.write(profileJson, json.toString().getBytes(StandardCharsets.UTF_8));
+
+		Path libsDir = mcDir.resolve("libraries");
+
+		for (Json libraryJson : json.at("libraries").asJsonList()) {
+			Library library = new Library(libraryJson);
+			Path libraryFile = libsDir.resolve(library.getPath());
+			String url = library.getURL();
+
+			progress.updateProgress(new MessageFormat(Utils.BUNDLE.getString("progress.download.library.entry")).format(new Object[]{library.name}));
+			FabricService.downloadSubstitutedMaven(url, libraryFile);
+		}
+
+		progress.updateProgress(Utils.BUNDLE.getString("progress.done"));
+
+		return profileName;
+	}
+}
